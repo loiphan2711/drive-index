33411f14b560f161d3bd434ec22d4469e9134a8f
loiphan2711 <loiph.dev2711@gmail.com>
2026-04-04 19:06:33 +0700

    feat: add eslint and convention for commitlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,65 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from '@antfu/eslint-config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default antfu(
+  {
+    react: true,
+    nextjs: true,
+    typescript: true,
 
-export default eslintConfig;
+    // Configuration preferences
+    lessOpinionated: true,
+    isInEditor: false,
+
+    // Code style
+    stylistic: {
+      semi: true,
+    },
+
+    // Format settings
+    formatters: {
+      css: true,
+    },
+
+    // Ignored paths
+    ignores: [
+      'src/migrations/**/*',
+      'src/payload-types.ts',
+      'src/app/(payload)/admin/importMap.js',
+      'src/**/*.scss',
+    ],
+  },
+  // --- Accessibility Rules ---
+  jsxA11y.flatConfigs.recommended,
+  // --- E2E Testing Rules ---
+  {
+    files: ['**/*.spec.ts', '**/*.e2e.ts'],
+  },
+  // --- Prettier Rules ---
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    },
+  },
+  // --- Custom Rule Overrides ---
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'antfu/no-top-level-await': 'off', // Allow top-level await
+      'style/brace-style': ['error', '1tbs'], // Use the default brace style
+      'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
+      'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+      'node/prefer-global/process': 'off', // Allow using `process.env`
+      'test/padding-around-all': 'error', // Add padding in test files
+      'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles,
+      'react/no-array-index-key': 'off',
+      'react-hooks-extra/no-direct-set-state-in-use-effect': 'off',
+    },
+  },
+);
